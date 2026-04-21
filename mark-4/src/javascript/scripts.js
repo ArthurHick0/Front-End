ca0ec4dd2c5e9763533758d96bfa6da233ca6350
Arthur Hick <arthurdelgado009@gmail.com>
2026-04-21 03:29:18 -0300

    Sidebar Lateral - finalizado

--- mark-4/src/javascript/scripts.js
+++ mark-4/src/javascript/scripts.js
@@ -5,9 +5,11 @@ const overlay = document.getElementById("overlay");
 btn.addEventListener("click", () => {
     sidebar.classList.toggle("open-sidebar");
     overlay.classList.toggle("active");
+    document.body.classList.toggle("sidebar-open");
 });
 
 overlay.addEventListener("click", () => {
     sidebar.classList.remove("open-sidebar");
     overlay.classList.remove("active");
+    document.body.classList.remove("sidebar-open");
 });
