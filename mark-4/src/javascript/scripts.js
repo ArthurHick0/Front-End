65fd08f45fc163c07f5398405f8cf3e124829872
Arthur Hick <arthurdelgado009@gmail.com>
2026-04-20 23:50:17 -0300

    Atualização sidebar lateral

--- mark-4/src/javascript/scripts.js
+++ mark-4/src/javascript/scripts.js
@@ -1,3 +1,13 @@
-document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
+const btn = document.getElementById("open_btn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+btn.addEventListener("click", () => {
+    sidebar.classList.toggle("open-sidebar");
+    overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+    sidebar.classList.remove("open-sidebar");
+    overlay.classList.remove("active");
 });
